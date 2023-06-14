989bc9a0dec4ea8dce0e15e9ee1ef0fbb6c065e5
Natalia-Si <n.sidoryk@tlen.pl>
2023-06-14 10:42:42 +0200

    Poczatkowa konfiguracja

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       Application
+      </div>
+    </>
+  )
+}
+
+export default App
